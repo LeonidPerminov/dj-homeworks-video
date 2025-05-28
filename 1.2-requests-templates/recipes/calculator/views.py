39ad8bc788219940448bf858c849939c9f0a4915
Leonid Perminov <lperminov@mail.ru>
2025-05-28 13:39:09 +0300

    Выполнено задание по шаблонам и views

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
 
 DATA = {
     'omlet': {
@@ -16,8 +17,36 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
+
+
+def recipe_view(request, dish):
+    servings = request.GET.get('servings', 1)
+    try:
+        servings = int(servings)
+        if servings < 1:
+            raise ValueError
+    except ValueError:
+        return HttpResponseBadRequest("Параметр servings должен быть положительным числом")
+
+    # Получаем рецепт или пустой словарь, если такого блюда нет
+    base_recipe = DATA.get(dish, {})
+
+    # Масштабируем
+    recipe = {
+        ingredient: amount * servings
+        for ingredient, amount in base_recipe.items()
+    }
+
+    context = {
+        'recipe': recipe
+    }
+
+    return render(request, 'calculator/index.html', context)
+
+
+    # можете добавить свои рецепты ;)
+
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)

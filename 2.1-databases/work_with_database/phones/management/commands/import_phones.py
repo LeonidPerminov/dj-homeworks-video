c2d6239cd4db94227a0526c6095a98282f57e966
Leonid Perminov <lperminov@mail.ru>
2025-06-03 18:22:30 +0300

    авершено задание по импорту телефонов из CSV

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,16 +2,28 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+import csv
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
-
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        with open('phones.csv', 'r', encoding='utf-8') as file:
+            phones = csv.DictReader(file, delimiter=';')
+            for phone in phones:
+                Phone.objects.create(
+                    id=int(phone['id']),
+                    name=phone['name'],
+                    image=phone['image'],
+                    price=int(phone['price']),
+                    release_date=phone['release_date'],
+                    lte_exists=phone['lte_exists'].lower() == 'true',
+                    slug=slugify(phone['name'])
+                )
+        self.stdout.write(self.style.SUCCESS('✅ Данные успешно импортированы'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+
+
+
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    image = models.URLField()
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+

dcb09521ff14612c72e56f3d2850f0103564b089
Leonid Perminov <lperminov@mail.ru>
2025-06-15 13:11:07 +0300

    бновлён проект: ScopeInline, шаблон, Pillow, всё работает

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Tag, Article, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_count = sum(1 for form in self.forms
+                         if not form.cleaned_data.get('DELETE', False) and form.cleaned_data.get('is_main'))
+        if main_count != 1:
+            raise ValidationError('Должен быть один и только один основной раздел.')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass

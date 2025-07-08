807adaf18bc3453247f5d4ed7bca1c4f5ab6421f
Leonid Perminov <lperminov@mail.ru>
2025-07-08 20:25:26 +0300

    обавлены файлы задания 2.1-databases

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Phone
+
+def index(request):
+    return redirect('catalog')
 
 
-def books_view(request):
-    template = 'books/books_list.html'
-    context = {}
-    return render(request, template, context)
+def show_catalog(request):
+    sort_param = request.GET.get('sort')
+    phones = Phone.objects.all()
+
+    if sort_param in ['name', 'price', '-price']:
+        phones = phones.order_by(sort_param)
+
+    return render(request, 'catalog.html', {'phones': phones})
+
+
+def show_product(request, slug):
+    phone = get_object_or_404(Phone, slug=slug)
+    return render(request, 'product.html', {'phone': phone})
